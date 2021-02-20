b5166269b834a9e41e3ada762748608551de78d6
Ashur Cabrera <hello@ashur.cab>
2021-02-20 14:45:38 -0700

    Switch posts to use Documents

--- src/_data/posts.js
+++ src/_data/posts.js
@@ -1,6 +1,5 @@
-const Cache = require( "@11ty/eleventy-cache-assets" );
 const dotenv = require( "dotenv" );
-const Portway = require( "../_scripts/portway" );
+const Documents = require( "../_scripts/documents" );
 const Post = require( "../_scripts/post" );
 
 /*
@@ -16,91 +15,61 @@ dotenv.config();
 module.exports = async () =>
 {
 	let requiredVarnames = [
-		"PORTWAY_KEY",
-		"PORTWAY_PROJECT_ID",
+		"PORTWAY_POSTS_ID",
+		"PORTWAY_POSTS_KEY",
 	];
 
-	// Don't fail the build if .env vars are
-	// missing. Just log a warning and return an
-	// empty array instead.
 	for( let varname of requiredVarnames )
 	{
 		if( !process.env[varname] )
 		{
-			console.log( `⚠️ Missing required environment variable '${varname}'.` );
+			console.log( `⚠️  Environment variable '${varname}' not found.` );
 			return [];
 		}
 	}
 
-	/* Posts */
-	let projectId = process.env.PORTWAY_PROJECT_ID;
-	let endpointDocuments = `projects/${projectId}/documents`;
-
-	let urlOptions = {};
-	if( process.env.PORTWAY_SHOW_DRAFTS === "true" )
+	let showDrafts = process.env.PORTWAY_SHOW_DRAFTS === "true";
+	if( showDrafts )
 	{
-		console.log( "⚠️  Including drafts" );
-		urlOptions.params = {
-			draft: true,
-		};
+		console.log( "⚠️  Including unpublished posts" );
 	};
 
 	try
 	{
-		let projectDocuments = await Portway.fetch(
-			process.env.PORTWAY_KEY,
-			endpointDocuments,
-			urlOptions
-		);
-
-		if( !projectDocuments.data )
-		{
-			throw new Error( "Unexpected response from Portway:", projectDocuments );
-		}
-
-		let populatedDocuments = projectDocuments.data.map( async (projectDocument) =>
-		{
-			let endpointPopulatedDocument = `documents/${projectDocument.id}/`;
-
-			return Portway.fetch(
-				process.env.PORTWAY_KEY,
-				endpointPopulatedDocument,
-				urlOptions
-			);
+		let populatedDocuments = await Documents.fetch({
+			apiKey: process.env.PORTWAY_POSTS_KEY,
+			projectId: process.env.PORTWAY_POSTS_ID,
+			showDrafts: showDrafts,
 		});
 
-		return Promise.all( populatedDocuments )
-			.then( populatedDocuments =>
+		return populatedDocuments
+			.map( populatedDocument =>
 			{
-				return populatedDocuments
-					.map( populatedDocument =>
-					{
-						if( !populatedDocument.data )
-						{
-							throw new Error( "Unexpected response from Portway:", populatedDocument );
-						}
-	
-						return new Post( populatedDocument.data );
-					})
-					.sort( (docA, docB) =>
-					{
-						// If only one item is pinned, sort it before the unpinned item
-						if( docA.data.pinned !== "true" || docB.data.pinned !== "true" )
-						{
-							if( docA.data.pinned === "true" )
-							{
-								return -1;
-							}
-							else if( docB.data.pinned === "true" )
-							{
-								return 1;
-							}
-						}
+				if( !populatedDocument.data )
+				{
+					throw new Error( "Unexpected response from Portway:", populatedDocument );
+				}
 
-						// Otherwise, fall through to date-based sorting
-						// (including cases where both items are pinned)
-						return docB.date - docA.date;
-					});
+				return new Post( populatedDocument.data );
+			})
+			.sort( (docA, docB) =>
+			{
+				// If only one item is pinned, sort it before the unpinned item
+				if( docA.data.pinned !== "true" || docB.data.pinned !== "true" )
+				{
+					if( docA.data.pinned === "true" )
+					{
+						return -1;
+					}
+					else if( docB.data.pinned === "true" )
+					{
+						return 1;
+					}
+				}
+
+				// Otherwise, fall through to date-based sorting
+				// (including cases where both items are pinned)
+				return docB.date - docA.date;
 			});
 	}
 	catch( error )
